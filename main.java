1c7be6cf7ed437859cbba5be783c3ba083ef8f05
Shahrukh <shahrukh.zhcet@gmail.com>
2024-02-06 20:15:25 +0530

    new commit

--- main.java
+++ main.java
@@ -4,5 +4,6 @@ public class main{
     public static void main(String[] args) {
         System.out.println("this is the java program");
         System.out.println(new Date());
+        System.out.println("added new commit");
     }
 }
