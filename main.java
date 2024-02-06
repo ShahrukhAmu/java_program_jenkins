833349db800785c43b0c99c71342ec4b6fe93d2e
Shahrukh <shahrukh.zhcet@gmail.com>
2024-02-06 20:04:17 +0530

    Jenkins_project

---
+++ main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class main{
+    public static void main(String[] args) {
+        System.out.println("this is the java program");
+        System.out.println(new Date());
+    }
+}
